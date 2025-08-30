e4c289c61a6ec2414a651bb2a16ab75672fafaab
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-08-30 12:59:34 +0530

    .

---
+++ src/main/java/org/vimal/security/v3/V3Application.java
@@ -0,0 +1,13 @@
+package org.vimal.security.v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V3Application.class, args);
+	}
+
+}
