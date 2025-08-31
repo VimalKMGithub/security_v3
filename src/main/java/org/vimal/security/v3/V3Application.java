0f072c9518f611abde9a7d13b09d773363f850db
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-08-31 13:46:30 +0530

    .

--- src/main/java/org/vimal/security/v3/V3Application.java
+++ src/main/java/org/vimal/security/v3/V3Application.java
@@ -2,12 +2,16 @@ package org.vimal.security.v3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class V3Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(V3Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(V3Application.class, args);
+    }
 }
