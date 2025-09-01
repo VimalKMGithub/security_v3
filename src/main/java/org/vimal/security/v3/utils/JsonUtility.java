dc58827c2cfb5cc107cbf635e3250b611cc49900
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-01 11:21:08 +0530

    .

---
+++ src/main/java/org/vimal/security/v3/utils/JsonUtility.java
@@ -0,0 +1,13 @@
+package org.vimal.security.v3.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JsonUtility {
+    private static final ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+
+    public static String toJson(Object obj) throws JsonProcessingException {
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+    }
+}
