3aa70938c3c2c6b066884db91a290ebda93a3d9c
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-04 11:52:09 +0530

    .

--- src/main/java/org/vimal/security/v3/utils/MfaUtility.java
+++ src/main/java/org/vimal/security/v3/utils/MfaUtility.java
@@ -3,15 +3,23 @@ package org.vimal.security.v3.utils;
 import org.vimal.security.v3.enums.MfaType;
 import org.vimal.security.v3.exceptions.SimpleBadRequestException;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public final class MfaUtility {
     private MfaUtility() {
     }
 
-    public static final Set<String> MFA_METHODS = Arrays.stream(MfaType.values()).map(e -> e.name().toLowerCase()).collect(Collectors.toSet());
+    public static final Set<String> MFA_METHODS = buildMfaMethodsSet();
+
+    private static Set<String> buildMfaMethodsSet() {
+        Set<String> methods = new HashSet<>();
+        for (MfaType type : MfaType.values()) {
+            methods.add(type.name().toLowerCase());
+        }
+        return Collections.unmodifiableSet(methods);
+    }
 
     public static void validateTypeExistence(String type) {
         if (!MFA_METHODS.contains(type.toLowerCase())) {
