3a92ab071bdd28e0214f52c6efd4fb277c7c2ec9
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-06 02:54:18 +0530

    .

--- src/main/java/org/vimal/security/v3/controllers/AuthenticationController.java
+++ src/main/java/org/vimal/security/v3/controllers/AuthenticationController.java
@@ -27,47 +27,81 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestParam String usernameOrEmail, @RequestParam String password) throws InvalidAlgorithmParameterException, JoseException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(authenticationService.login(usernameOrEmail, password));
+    public ResponseEntity<Map<String, Object>> login(@RequestParam String usernameOrEmail,
+                                                     @RequestParam String password)
+            throws InvalidAlgorithmParameterException, JoseException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(authenticationService.login(
+                        usernameOrEmail,
+                        password
+                )
+        );
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Map<String, String>> logout() throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> logout()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(authenticationService.logout());
     }
 
     @PostMapping("/refresh/accessToken")
-    public ResponseEntity<Map<String, Object>> refreshAccessToken(@RequestParam String refreshToken) throws InvalidAlgorithmParameterException, JoseException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> refreshAccessToken(@RequestParam String refreshToken)
+            throws InvalidAlgorithmParameterException, JoseException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(authenticationService.refreshAccessToken(refreshToken));
     }
 
     @PostMapping("/revoke/accessToken")
-    public ResponseEntity<Map<String, String>> revokeAccessToken() throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> revokeAccessToken()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(authenticationService.revokeAccessToken());
     }
 
     @PostMapping("/revoke/refreshToken")
-    public ResponseEntity<Map<String, String>> revokeRefreshToken(@RequestParam String refreshToken) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> revokeRefreshToken(@RequestParam String refreshToken)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(authenticationService.revokeRefreshToken(refreshToken));
     }
 
     @PostMapping("/mfa/requestTo/toggle")
-    public ResponseEntity<Object> requestToToggleMfa(@RequestParam String type, @RequestParam String toggle) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, IOException, InvalidKeyException, WriterException {
+    public ResponseEntity<Object> requestToToggleMfa(@RequestParam String type,
+                                                     @RequestParam String toggle)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, IOException, InvalidKeyException, WriterException {
         return authenticationService.requestToToggleMfa(type, toggle);
     }
 
     @PostMapping("/mfa/verifyTo/toggle")
-    public ResponseEntity<Map<String, String>> verifyToggleMfa(@RequestParam String type, @RequestParam String toggle, @RequestParam String otpTotp) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(authenticationService.verifyToggleMfa(type, toggle, otpTotp));
+    public ResponseEntity<Map<String, String>> verifyToggleMfa(@RequestParam String type,
+                                                               @RequestParam String toggle,
+                                                               @RequestParam String otpTotp)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(authenticationService.verifyToggleMfa(
+                        type,
+                        toggle,
+                        otpTotp
+                )
+        );
     }
 
     @PostMapping("/mfa/requestTo/login")
-    public ResponseEntity<Map<String, String>> requestToLoginMfa(@RequestParam String type, @RequestParam String stateToken) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(authenticationService.requestToLoginMfa(type, stateToken));
+    public ResponseEntity<Map<String, String>> requestToLoginMfa(@RequestParam String type,
+                                                                 @RequestParam String stateToken)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(authenticationService.requestToLoginMfa(
+                        type,
+                        stateToken
+                )
+        );
     }
 
     @PostMapping("/mfa/verifyTo/login")
-    public ResponseEntity<Map<String, Object>> verifyMfaToLogin(@RequestParam String type, @RequestParam String stateToken, @RequestParam String otpTotp) throws InvalidAlgorithmParameterException, JoseException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(authenticationService.verifyMfaToLogin(type, stateToken, otpTotp));
+    public ResponseEntity<Map<String, Object>> verifyMfaToLogin(@RequestParam String type,
+                                                                @RequestParam String stateToken,
+                                                                @RequestParam String otpTotp)
+            throws InvalidAlgorithmParameterException, JoseException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(authenticationService.verifyMfaToLogin(
+                        type,
+                        stateToken,
+                        otpTotp
+                )
+        );
     }
 }
