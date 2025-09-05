08fe38909a793f14cf9b042efc4deac0a5cf66c1
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-06 02:58:07 +0530

    .

--- src/main/java/org/vimal/security/v3/controllers/UserController.java
+++ src/main/java/org/vimal/security/v3/controllers/UserController.java
@@ -24,82 +24,115 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, Object>> register(@RequestBody RegistrationDto dto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> register(@RequestBody RegistrationDto dto)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.register(dto);
     }
 
     @GetMapping("/getSelfDetails")
-    public ResponseEntity<UserSummaryDto> getSelfDetails() throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<UserSummaryDto> getSelfDetails()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(userService.getSelfDetails());
     }
 
     @PostMapping("/verifyEmail")
-    public ResponseEntity<Map<String, Object>> verifyEmail(@RequestParam String emailVerificationToken) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> verifyEmail(@RequestParam String emailVerificationToken)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(userService.verifyEmail(emailVerificationToken));
     }
 
     @PostMapping("/resend/emailVerification/link")
-    public ResponseEntity<Map<String, String>> resendEmailVerificationLink(@RequestParam String usernameOrEmail) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> resendEmailVerificationLink(@RequestParam String usernameOrEmail)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(userService.resendEmailVerificationLink(usernameOrEmail));
     }
 
     @PostMapping("/forgot/password")
-    public ResponseEntity<Map<String, Object>> forgotPassword(@RequestParam String usernameOrEmail) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> forgotPassword(@RequestParam String usernameOrEmail)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.forgotPassword(usernameOrEmail);
     }
 
     @PostMapping("/forgot/password/methodSelection")
-    public ResponseEntity<Map<String, String>> forgotPasswordMethodSelection(@RequestParam String usernameOrEmail, @RequestParam(defaultValue = DEFAULT_MFA) String method) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(userService.forgotPasswordMethodSelection(usernameOrEmail, method));
+    public ResponseEntity<Map<String, String>> forgotPasswordMethodSelection(@RequestParam String usernameOrEmail,
+                                                                             @RequestParam(defaultValue = DEFAULT_MFA) String method)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(userService.forgotPasswordMethodSelection(
+                        usernameOrEmail,
+                        method
+                )
+        );
     }
 
     @PostMapping("/reset/password")
-    public ResponseEntity<Map<String, Object>> resetPassword(@RequestBody ResetPwdDto dto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> resetPassword(@RequestBody ResetPwdDto dto)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.resetPassword(dto);
     }
 
     @PostMapping("/change/password")
-    public ResponseEntity<Map<String, Object>> changePassword(@RequestBody ChangePwdDto dto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> changePassword(@RequestBody ChangePwdDto dto)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.changePassword(dto);
     }
 
     @PostMapping("/change/password/methodSelection")
-    public ResponseEntity<Map<String, String>> changePasswordMethodSelection(@RequestParam String method) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> changePasswordMethodSelection(@RequestParam String method)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(userService.changePasswordMethodSelection(method));
     }
 
     @PostMapping("/verify/change/password")
-    public ResponseEntity<Map<String, Object>> verifyChangePassword(@RequestBody ChangePwdDto dto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> verifyChangePassword(@RequestBody ChangePwdDto dto)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.verifyChangePassword(dto);
     }
 
     @PostMapping("/email/change/request")
-    public ResponseEntity<Map<String, String>> emailChangeRequest(@RequestParam String newEmail) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> emailChangeRequest(@RequestParam String newEmail)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(userService.emailChangeRequest(newEmail));
     }
 
     @PostMapping("/verify/email/change")
-    public ResponseEntity<Map<String, Object>> verifyEmailChange(@RequestParam String newEmailOtp, @RequestParam String oldEmailOtp, @RequestParam String password) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(userService.verifyEmailChange(newEmailOtp, oldEmailOtp, password));
+    public ResponseEntity<Map<String, Object>> verifyEmailChange(@RequestParam String newEmailOtp,
+                                                                 @RequestParam String oldEmailOtp,
+                                                                 @RequestParam String password)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(userService.verifyEmailChange(
+                        newEmailOtp,
+                        oldEmailOtp,
+                        password
+                )
+        );
     }
 
     @DeleteMapping("/delete/account")
-    public ResponseEntity<Map<String, Object>> deleteAccount(@RequestParam String password) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> deleteAccount(@RequestParam String password)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.deleteAccount(password);
     }
 
     @PostMapping("/delete/account/methodSelection")
-    public ResponseEntity<Map<String, String>> deleteAccountMethodSelection(@RequestParam String method) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, String>> deleteAccountMethodSelection(@RequestParam String method)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return ResponseEntity.ok(userService.deleteAccountMethodSelection(method));
     }
 
     @DeleteMapping("/verify/delete/account")
-    public ResponseEntity<Map<String, String>> verifyDeleteAccount(@RequestParam String otpTotp, @RequestParam String method) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
-        return ResponseEntity.ok(userService.verifyDeleteAccount(otpTotp, method));
+    public ResponseEntity<Map<String, String>> verifyDeleteAccount(@RequestParam String otpTotp,
+                                                                   @RequestParam String method)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+        return ResponseEntity.ok(userService.verifyDeleteAccount(
+                        otpTotp,
+                        method
+                )
+        );
     }
 
     @PutMapping("/update/details")
-    public ResponseEntity<Map<String, Object>> updateDetails(@RequestBody SelfUpdationDto dto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> updateDetails(@RequestBody SelfUpdationDto dto)
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, JsonProcessingException {
         return userService.updateDetails(dto);
     }
 }
