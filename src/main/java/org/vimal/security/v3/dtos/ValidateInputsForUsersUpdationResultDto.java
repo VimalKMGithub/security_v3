cd950e0e77c4c739b0cd6262bbbda4e4142485a4
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-06 13:07:03 +0530

    .

--- src/main/java/org/vimal/security/v3/dtos/ValidateInputsForUsersUpdationResultDto.java
+++ src/main/java/org/vimal/security/v3/dtos/ValidateInputsForUsersUpdationResultDto.java
@@ -1,38 +1,55 @@
-//package org.vimal.security.v3.dtos;
-//
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.util.Map;
-//import java.util.Set;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//public class ValidateInputsForUsersUpdationResultDto extends ValidateInputsForUsersCreationResultDto {
-//    private Set<String> encryptedOldUsernames;
-//    private Map<String, String> encryptedOldUsernameToOldUsernameMap;
-//    private Set<String> duplicateOldUsernames;
-//    private Set<String> invalidOldUsernames;
-//
-//    public ValidateInputsForUsersUpdationResultDto(Set<String> invalidInputs,
-//                                                   Set<String> encryptedUsernames,
-//                                                   Map<String, String> encryptedUsernameToUsernameMap,
-//                                                   Set<String> encryptedEmails,
-//                                                   Map<String, String> encryptedEmailToEmailMap,
-//                                                   Set<String> duplicateUsernamesInDtos,
-//                                                   Set<String> duplicateEmailsInDtos,
-//                                                   Set<String> roles,
-//                                                   Set<String> restrictedRoles,
-//                                                   Set<String> encryptedOldUsernames,
-//                                                   Map<String, String> encryptedOldUsernameToOldUsernameMap,
-//                                                   Set<String> duplicateOldUsernames,
-//                                                   Set<String> invalidOldUsernames) {
-//        super(invalidInputs, encryptedUsernames, encryptedUsernameToUsernameMap, encryptedEmails, encryptedEmailToEmailMap, duplicateUsernamesInDtos, duplicateEmailsInDtos, roles, restrictedRoles);
-//        this.encryptedOldUsernames = encryptedOldUsernames;
-//        this.encryptedOldUsernameToOldUsernameMap = encryptedOldUsernameToOldUsernameMap;
-//        this.duplicateOldUsernames = duplicateOldUsernames;
-//        this.invalidOldUsernames = invalidOldUsernames;
-//    }
-//}
+package org.vimal.security.v3.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ValidateInputsForUsersUpdationResultDto extends ValidateInputsForUsersCreationResultDto {
+    private Set<String> encryptedOldUsernames;
+    private Map<String, String> encryptedOldUsernameToOldUsernameMap;
+    private Map<String, String> oldUsernameToEncryptedOldUsernameMap;
+    private Set<String> duplicateOldUsernames;
+    private Set<String> invalidOldUsernames;
+
+    public ValidateInputsForUsersUpdationResultDto(Set<String> invalidInputs,
+                                                   Set<String> encryptedUsernames,
+                                                   Map<String, String> encryptedUsernameToUsernameMap,
+                                                   Map<String, String> usernameToEncryptedUsernameMap,
+                                                   Set<String> encryptedEmails,
+                                                   Map<String, String> encryptedEmailToEmailMap,
+                                                   Map<String, String> emailToEncryptedEmailMap,
+                                                   Set<String> duplicateUsernamesInDtos,
+                                                   Set<String> duplicateEmailsInDtos,
+                                                   Set<String> roles,
+                                                   Set<String> restrictedRoles,
+                                                   Set<String> encryptedOldUsernames,
+                                                   Map<String, String> encryptedOldUsernameToOldUsernameMap,
+                                                   Map<String, String> oldUsernameToEncryptedOldUsernameMap,
+                                                   Set<String> duplicateOldUsernames,
+                                                   Set<String> invalidOldUsernames) {
+        super(
+                invalidInputs,
+                encryptedUsernames,
+                encryptedUsernameToUsernameMap,
+                usernameToEncryptedUsernameMap,
+                encryptedEmails,
+                encryptedEmailToEmailMap,
+                emailToEncryptedEmailMap,
+                duplicateUsernamesInDtos,
+                duplicateEmailsInDtos,
+                roles,
+                restrictedRoles
+        );
+        this.encryptedOldUsernames = encryptedOldUsernames;
+        this.encryptedOldUsernameToOldUsernameMap = encryptedOldUsernameToOldUsernameMap;
+        this.oldUsernameToEncryptedOldUsernameMap = oldUsernameToEncryptedOldUsernameMap;
+        this.duplicateOldUsernames = duplicateOldUsernames;
+        this.invalidOldUsernames = invalidOldUsernames;
+    }
+}
