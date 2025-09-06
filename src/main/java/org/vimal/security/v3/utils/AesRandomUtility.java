c34d8f2ed1e8becfe42414fd81dfff9264ae2f16
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-06 10:46:28 +0530

    .

--- src/main/java/org/vimal/security/v3/utils/AesRandomUtility.java
+++ src/main/java/org/vimal/security/v3/utils/AesRandomUtility.java
@@ -17,31 +17,56 @@ public class AesRandomUtility {
     private final ObjectMapper objectMapper;
     private final SecretKey secretKey;
 
-    public AesRandomUtility(String aesSecret, ObjectMapper objectMapper) throws NoSuchAlgorithmException {
-        this.secretKey = new SecretKeySpec(MessageDigest.getInstance("SHA-256").digest(aesSecret.getBytes()), "AES");
+    public AesRandomUtility(String aesSecret,
+                            ObjectMapper objectMapper) throws NoSuchAlgorithmException {
+        this.secretKey = new SecretKeySpec(MessageDigest.getInstance("SHA-256")
+                .digest(aesSecret.getBytes()),
+                "AES");
         this.objectMapper = objectMapper;
     }
 
-    public String encrypt(Object data) throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public String encrypt(Object data)
+            throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         return encryptString(objectMapper.writeValueAsString(data));
     }
 
-    private String encryptString(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private String encryptString(String data)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         byte[] iv = new byte[GCM_IV_LENGTH];
         secureRandom.nextBytes(iv);
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new GCMParameterSpec(GCM_TAG_LENGTH, iv));
-        return Base64.getEncoder().encodeToString(iv) + ":" + Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
+        cipher.init(
+                Cipher.ENCRYPT_MODE, secretKey,
+                new GCMParameterSpec(
+                        GCM_TAG_LENGTH,
+                        iv
+                )
+        );
+        return Base64.getEncoder()
+                .encodeToString(iv) + ":" + Base64.getEncoder()
+                .encodeToString(cipher.doFinal(data.getBytes()));
     }
 
-    public <T> T decrypt(String encryptedData, Class<T> targetClass) throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public <T> T decrypt(String encryptedData,
+                         Class<T> targetClass)
+            throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         return objectMapper.readValue(decryptString(encryptedData), targetClass);
     }
 
-    private String decryptString(String encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private String decryptString(String encryptedData)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         String[] parts = encryptedData.split(":");
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, new GCMParameterSpec(GCM_TAG_LENGTH, Base64.getDecoder().decode(parts[0])));
-        return new String(cipher.doFinal(Base64.getDecoder().decode(parts[1])));
+        cipher.init(
+                Cipher.DECRYPT_MODE,
+                secretKey,
+                new GCMParameterSpec(
+                        GCM_TAG_LENGTH,
+                        Base64.getDecoder()
+                                .decode(parts[0])
+                )
+        );
+        return new String(cipher.doFinal(Base64.getDecoder()
+                .decode(parts[1])));
     }
 }
--- src/main/java/org/vimal/security/v3/utils/AesStaticUtility.java
+++ src/main/java/org/vimal/security/v3/utils/AesStaticUtility.java
@@ -15,31 +15,53 @@ import java.util.Base64;
 public class AesStaticUtility {
     private static final String TRANSFORMATION = "AES/CBC/PKCS5Padding";
     private static final byte[] FIXED_IV = new byte[16];
+    private static final IvParameterSpec FIXED_IV_SPEC = new IvParameterSpec(FIXED_IV);
     private final ObjectMapper objectMapper;
     private final SecretKey secretKey;
 
-    public AesStaticUtility(String aesSecret, ObjectMapper objectMapper) throws NoSuchAlgorithmException {
-        this.secretKey = new SecretKeySpec(MessageDigest.getInstance("SHA-256").digest(aesSecret.getBytes()), "AES");
+    public AesStaticUtility(String aesSecret,
+                            ObjectMapper objectMapper) throws NoSuchAlgorithmException {
+        this.secretKey = new SecretKeySpec(MessageDigest.getInstance("SHA-256")
+                .digest(aesSecret.getBytes()),
+                "AES");
         this.objectMapper = objectMapper;
     }
 
-    public String encrypt(Object data) throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public String encrypt(Object data)
+            throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         return encryptString(objectMapper.writeValueAsString(data));
     }
 
-    private String encryptString(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private String encryptString(String data)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(FIXED_IV));
-        return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
+        cipher.init(
+                Cipher.ENCRYPT_MODE,
+                secretKey,
+                FIXED_IV_SPEC
+        );
+        return Base64.getEncoder()
+                .encodeToString(cipher.doFinal(data.getBytes()));
     }
 
-    public <T> T decrypt(String encryptedData, Class<T> targetClass) throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-        return objectMapper.readValue(decryptString(encryptedData), targetClass);
+    public <T> T decrypt(String encryptedData,
+                         Class<T> targetClass)
+            throws JsonProcessingException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        return objectMapper.readValue(
+                decryptString(encryptedData),
+                targetClass
+        );
     }
 
-    private String decryptString(String encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    private String decryptString(String encryptedData)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(FIXED_IV));
-        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedData)));
+        cipher.init(
+                Cipher.DECRYPT_MODE,
+                secretKey,
+                FIXED_IV_SPEC
+        );
+        return new String(cipher.doFinal(Base64.getDecoder()
+                .decode(encryptedData)));
     }
 }
